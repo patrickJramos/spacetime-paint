8cc9308dbfb94cb9a045734e357b12c399ed5a75
patrick.ramos <patrick.ramos@marvee.com.br>
2025-04-10 19:31:55 -0300

    added client

---
+++ client/src/App.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+export type PrettyMessage = {
+  senderName: string;
+  text: string;
+};
+
+function App() {
+  const [newName, setNewName] = useState('');
+  const [settingName, setSettingName] = useState(false);
+  const [systemMessage, setSystemMessage] = useState('');
+  const [newMessage, setNewMessage] = useState('');
+
+  const prettyMessages: PrettyMessage[] = [];
+
+  const name = '';
+
+  const onSubmitNewName = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSettingName(false);
+    // TODO: Call `setName` reducer
+  };
+
+  const onMessageSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setNewMessage('');
+    // TODO: Call `sendMessage` reducer
+  };
+
+  return (
+    <div className="App">
+      <div className="profile">
+        <h1>Profile</h1>
+        {!settingName ? (
+          <>
+            <p>{name}</p>
+            <button
+              onClick={() => {
+                setSettingName(true);
+                setNewName(name);
+              }}
+            >
+              Edit Name
+            </button>
+          </>
+        ) : (
+          <form onSubmit={onSubmitNewName}>
+            <input
+              type="text"
+              value={newName}
+              onChange={e => setNewName(e.target.value)}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </div>
+      <div className="message">
+        <h1>Messages</h1>
+        {prettyMessages.length < 1 && <p>No messages</p>}
+        <div>
+          {prettyMessages.map((message, key) => (
+            <div key={key}>
+              <p>
+                <b>{message.senderName}</b>
+              </p>
+              <p>{message.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="system" style={{ whiteSpace: 'pre-wrap' }}>
+        <h1>System</h1>
+        <div>
+          <p>{systemMessage}</p>
+        </div>
+      </div>
+      <div className="new-message">
+        <form
+          onSubmit={onMessageSubmit}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '50%',
+            margin: '0 auto',
+          }}
+        >
+          <h3>New Message</h3>
+          <textarea
+            value={newMessage}
+            onChange={e => setNewMessage(e.target.value)}
+          ></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
